2ea295d8113a53fd4efb3ec4c83a47d5fd3b4550
jlumbres <jlumbres@optics.arizona.edu>
2020-07-15 00:39:26 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="model_kit", 
+    version="0.0.1.dev",
+    url="https://github.com/jlumbres/model_kit",
+    author="Jennifer Lumbres",
+    author_email="jlumbres@email.arizona.edu",
+    description="Testbed modeling tools",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
